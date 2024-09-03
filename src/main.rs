c139ce3bdd4b3d4cec45075197b57ee1f7f62f21
Helge Eight <w.iohansson@gmail.com>
2024-09-04 00:13:50 +0200

    feat: exec program

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,23 @@ use std::io::stdout;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    // read PATH environment variable
+fn input_to_args(input: &str) -> Vec<&str> {
+    input.trim().split_whitespace().collect()
+}
+
+fn find_path(command: &str) -> Option<String> {
     let path = std::env::var("PATH").unwrap_or_else(|_| "".to_string());
     let paths = path.split(":").collect::<Vec<&str>>();
+    for path in paths {
+        let path = format!("{}/{}", path, command);
+        if std::path::Path::new(&path).exists() {
+            return Some(path);
+        }
+    }
+    None
+}
+
+fn main() {
     loop {
         print!("$ ");
         stdout().flush().unwrap();
@@ -16,40 +29,42 @@ fn main() {
 
         stdin.read_line(&mut input).unwrap();
 
-        let command = input.trim().split_whitespace().next().unwrap();
+        let command = *input_to_args(&input).get(0).unwrap();
 
         match command {
             "echo" => {
                 // join all the arguments with a space
-                let args = input
-                    .trim()
-                    .split_whitespace()
-                    .skip(1)
-                    .collect::<Vec<&str>>()
-                    .join(" ");
+                let args = input_to_args(&input).get(1..).unwrap().join(" ");
                 println!("{}", args);
             }
             "type" => {
-                let command = input.trim().split_whitespace().skip(1).next().unwrap();
+                let command = *input_to_args(&input).get(1).unwrap();
                 if command == "echo" || command == "type" || command == "exit" {
                     println!("{} is a shell builtin", command);
                 } else {
-                    let mut found = false;
-                    for path in &paths {
-                        let path = format!("{}/{}", path, command);
-                        if std::path::Path::new(&path).exists() {
-                            println!("{} is {}", command, path);
-                            found = true;
-                            break;
-                        }
-                    }
-                    if !found {
-                        println!("{}: not found", command);
+                    let path = find_path(command);
+                    match path {
+                        Some(path) => println!("{} is {}", command, path),
+                        None => println!("{}: not found", command),
                     }
                 }
             }
             "exit" => break,
-            _ => println!("{}: command not found", command),
+            _ => {
+                let program = *input_to_args(&input).get(1).unwrap();
+                let args = input_to_args(&input).into_iter().skip(2);
+                let path = find_path(program);
+                match path {
+                    Some(path) => {
+                        let child_output = std::process::Command::new(path)
+                            .args(args)
+                            .output()
+                            .expect("failed to execute process");
+                        println!("{}", String::from_utf8_lossy(&child_output.stdout));
+                    }
+                    None => println!("{}: command not found", program),
+                }
+            }
         }
     }
 }
